8e47f384dc41187ebd2f16573becde2702c9fad1
RealCommonCat <real_common_cat@proton.me>
2023-07-23 15:47:27 +0800

    modified:   main.py 	data/

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from compare_with_dict import compare_with_dict
 from data_slice import slice_data 
 from multiprocessing import Lock 
 import multiprocessing
+SPLIT="@@@@@"
 class Main():
     def read_data(self):
         print("开始读取数据")
@@ -54,7 +55,7 @@ class Main():
             })
         block=lines[len(lines)-len(lines)%self.analyse_window:]
         if(len(block)>0):
-            out=out+lda_(block)
+            out=out+[SPLIT]+lda_(block)
         queue.put({
             "type":"completed",
             "process_idx":idx,
@@ -71,24 +72,35 @@ class Main():
         for p in ps:
             p.start()
             print(p)
-    def lda_bar_func(self,total_len,completed,name):
-        print_progress_bar(completed,total_len,prefix=f'{name} LDA Progress:', suffix='Complete')
+
     def get_processed_len(self,ps_):
         len_=0
         for p in ps_:
             len_+=len(p)
         return len_
+    
+    def print_completed_bar(self,condition,total):
+        c0=0
+        for c in condition["completed"]:
+            c0+=c
+        print_progress_bar(c0,total,prefix="LDA: ",suffix='Completed')
+    def results_process(self):
+        out2_files=os.listdir(self.out2)
+        for f in out2_files:
+            fs=open(self.out2+"/"+f,"r",encoding="utf-8")
+            
     def __init__(self):   
         self.lock=Lock()
         self.data=[]
         self.out1="./out1"
+        self.out2="./out2"
         self.reg_date=r"\d{4}-\d{2}-\d{2}\s\d{1,2}:\d{2}:\d{2}"
         self.k=0.1
         self.analyse_window=1000
         self.overlapping=1
         self.process_lda=[]
         self.data_processed=[]
-        self.amount=4
+        self.amount=2
         self.queue=multiprocessing.Queue()
         self.read_data()
         self.condition={
@@ -96,15 +108,25 @@ class Main():
             "completed": [0 for x in range(len(self.data))]
         }
         self.outs_=[[] for x in range(len(self.data))]
+        self.total=0
         for i in range(len(self.data)):
             self.data_processed.append({"data":slice_data(self.read_out1(self.data[i]["file"]),self.amount),"name":self.data[i]['name'],"file":self.data[i]["file"]})
+        for d in self.data_processed:
+            for d1 in d["data"]:
+                self.total+=len(d1)
         print("开始运行LDA")
-        for i in range(len(self.data)):
-            self.lda__multi(self.queue,self.data_processed[i]["data"],self.data_processed[i]["name"],self.data_processed[i]["file"],i)
+        out2_files=os.listdir(self.out2)
+        self.completed_out2=[]
+        for o in out2_files:
+            self.completed_out2.append(out2_files[:-4])
+        for i in range(len(self.data_processed)):
+            if(self.data_processed[i]["file"] not in self.completed_out2):
+                self.lda__multi(self.queue,self.data_processed[i]["data"],self.data_processed[i]["name"],self.data_processed[i]["file"],i)
+            else:
+                print(f"准备加载已完成的LDA文件{self.data_processed[i]['file']}")
         while(True):
             time.sleep(0.5)
-            for i in range(len(self.data)):
-                self.lda_bar_func(self.get_processed_len(self.data_processed[i]["data"]),self.condition["completed"][i],self.data_processed[i]["file"])
+            self.print_completed_bar(self.condition,self.total)
             m=self.queue.get()
             if(m["type"]=="lda"):
                 self.condition["completed"][m["s"]]+=self.overlapping*self.analyse_window
@@ -126,6 +148,6 @@ class Main():
                 if(k_):
                   break
         print("LDA完成 准备进行数据整理")
-            
+        #self.results_process()
 if __name__ == '__main__':
     main=Main()
